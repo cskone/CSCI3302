45762e1881cc71b8d37d24881c2d1fe1ec379a3c
cskone <cskone@users.noreply.github.com>
2021-02-26 20:38:04 -1000

    Update LogisticRegression.py

--- LogisticRegression.py
+++ LogisticRegression.py
@@ -17,8 +17,8 @@ dataset = [[1, 1, 0],
 
 b = [0 for j in range(len(dataset[0])-1)] # List containing θk
 b2 = [0 for j in range(len(dataset[0])-1)] # List containing θ'k
-learningRate = 0.003
-tolerance = 0.001
+learningRate = 0.003 # Learning Rate α 
+tolerance = 0.001 # Error value ϵ
 
 # Calculate hθ(X)
 def hypothesis(dataset, b):
@@ -31,7 +31,7 @@ def hypothesis(dataset, b):
 def probHypothesis(h):
     return 1 / (1 + math.exp(-h))
 
-# Calculate LL(y ∣ X, θ)
+# Calculate LL(y | X, θ)
 def calcLL(dataset, b):
     ll = 0
     for i in range(len(dataset)):
@@ -43,20 +43,25 @@ def calcLL(dataset, b):
     
     return ll
 
-# Puts everything together.  I could not get the do/while loop to work,
-# so I resorted to just using a for loop
+# Puts everything together.  Something in relation to LL is not working, so I am using a for loop to iterate.
+# The issue is that, somewhere, somehow, at iteration 3 and beyond θ and θ' are always the same value at the point
+# where LL is calculated.  This causes ΔLL = 0, making it useless to compare to ϵ.
 for x in range(100):
+#while True:
     hyp = hypothesis(dataset, b)
     pHyp = probHypothesis(hyp)
     for j in range(len(b2)):
         r = 0
         for i in range(len(dataset)):
-            r += (dataset[i][-1] - pHyp) * dataset[i][j] #partial derivative of LL(y ∣ X, θ)
+            r += (dataset[i][-1] - pHyp) * dataset[i][j] # Partial derivative of LL(y | X, θ)
         
         b2[j] = b[j] + (learningRate/len(dataset) * r) # θ'j := θj + α/n * r
+        print(learningRate/len(dataset) * r)
     
-    llDiff = calcLL(dataset, b) - calcLL(dataset, b2) #Set ΔLL  
+    llDiff = calcLL(dataset, b2) - calcLL(dataset, b)  # ΔLL := LL(y | X, θ') - LL(y | X, θ)
     b = b2 # θj := θ'j
+#    if llDiff < 0.001:
+#        break
 
 # Display expected outcome, the predicted outcome, and the probability of y = 1
 for i in range(len(dataset)):
